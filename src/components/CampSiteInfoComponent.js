69038e3bc69f7f95d9f6554b79ff96cc10496ff5
Magdalena Martinez <orangeclever@gmail.com>
2020-07-04 13:08:36 -0700

    Task 3 Finish

--- src/components/CampSiteInfoComponent.js
+++ src/components/CampSiteInfoComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from React;
+import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, } from 'reactstrap';
 
 
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from React;
+import React, { Component } from 'react';
 import { Card, CardImg, CardTitle, CardImgOverlay } from 'reactstrap';
 import CampsiteInfo from './CampSiteInfoComponent';
 
